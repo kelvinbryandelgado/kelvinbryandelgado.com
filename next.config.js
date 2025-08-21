0266849eb6496bb649cc882b668449a685ba7fa6
kelvinbryandelgado <kelvinbryandelgado@gmail.com>
2025-08-21 16:45:31 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: true },
   images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
-
-  // Let the site build even if ESLint/TS complains (we can fix later)
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
