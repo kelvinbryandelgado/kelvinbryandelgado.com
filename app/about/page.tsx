c5a901af8026bc8c733b24b5324aaf0731430199
kelvinbryandelgado <kelvinbryandelgado@gmail.com>
2025-08-21 17:00:52 -0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,1 +1,10 @@
-<div className='container py-10'><h1 className='h1'>About</h1><p className='mt-6'>Edit this page in <code>app/about/page.tsx</code>.</p></div>
+export default function AboutPage() {
+  return (
+    <div className="container py-10">
+      <h1 className="h1">About</h1>
+      <p className="mt-6">
+        Edit this page in <code>app/about/page.tsx</code>.
+      </p>
+    </div>
+  );
+}
